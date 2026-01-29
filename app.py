786e6ce22d539eb462ee6267732d3174bdbd4dd6
Mridul <mridulku225@gmail.com>
2026-01-29 23:16:42 +0530

    Deploy optimized model: 92.14% accuracy, 4.69MB size, removed unnecessary files

--- app.py
+++ app.py
@@ -121,6 +121,9 @@ def health():
         'scaler_loaded': scaler is not None
     })
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
